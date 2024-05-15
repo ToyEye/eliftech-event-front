6c13c8d00c2b95dae8e1a4cebf156583c5d33547
Korotenko Alex <55750546+ToyEye@users.noreply.github.com>
2024-05-15 09:02:31 +0300

    add server

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { useInView } from "react-intersection-observer";
 
 import { getEvents } from "../service/api";
